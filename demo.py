91e341c33abaae41eb3ca36be8a4747c71079ccf
DIRRY888 <864741720@qq.com>
2021-04-10 17:33:47 +0800

    维护
    维护

--- demo.py
+++ demo.py
@@ -4,13 +4,14 @@ from forms import *
 from database.database import *
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash
-import auth
+import auth, config
 import os
 
 # # from From import MyForm
 app = Flask(__name__, template_folder='Templates',instance_relative_config=True)
 # app.config['SECRET_KEY'] = 'project'
-app.secret_key = 'projectj'
+
+app.secret_key = 'project'
 
 app.config['SQLALCHEMY_DATABASE_URI']=os.getenv('DATABASE_URL', 'sqlite:///' + os.path.join(app.root_path, 'database.db'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
@@ -18,9 +19,10 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 # app.config.from_pyfile('config.py')
 app.debug = True
 
+
 db = SQLAlchemy(app)
 
-db.init_app(app)
+config.init_app(app)
 app.register_blueprint(auth.bp)
 
 
@@ -31,34 +33,45 @@ def hello_world():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    # form = MyForm()
-    # if form.validate_on_submit():  # 将会检查是否是一个 POST 请求并且请求是否有效。
-    #     return redirect(url_for(''))  # 如有有效就重定向到首页
-    # name = request.form.get('name')
-    # password = request.form.get('password')
-    # print(name)
-    # return render_template('rendering.html', form=form)
-    username = request.form['username']
-    password = request.form['password']
-    print(username)
-    print(password)
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        print(username)
+        print(password)
+        error = None
 
-    # 模拟验证
-    if username == 'admin' and password == '123':
-        session['name'] = username
-        return redirect(url_for('login'))
-    if username != 'admin':
-        flash('no this name')
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        # elif db.execute(
+        #         'SELECT id FROM user WHERE username = ?', (username,)
+        # ).fetchone() is not None:
+        #     error = 'User {} is already registered.'.format(username)
 
-    # 从数据库中验证信息
-    form=LoginForm()
-    account=Account.query.all()
-    if form.validate_on_sumbit():
-        if  account.email==form.email.data and account.username==form.username.data and account.password==form.password.data:
-            db.session.commit()
-
-
+        # if error is None:
+        #     db.execute(
+        #         'INSERT INTO user (username, password) VALUES (?, ?)',
+        #         (username, generate_password_hash(password))
+        #     )
+        #     db.commit()
+            return redirect(url_for('login'))
+        flash(error)
+        # 模拟验证
+        if username == 'admin' and password == '123':
+            session['name'] = username
+            return redirect(url_for('register'))
+        if username != 'admin':
+            flash('no this name')
     return render_template('login.html')
+    # # 从数据库中验证信息
+    # form=LoginForm()
+    # account=Account.query.all()
+    # if form.validate_on_sumbit():
+    #     if  account.email==form.email.data and account.username==form.username.data and account.password==form.password.data:
+    #         db.session.commit()
+
+
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -70,6 +83,7 @@ def register():
         print(email)
         print(username)
         print(password)
+        db = config.get_db()
         error = None
 
         if not username:
@@ -79,32 +93,25 @@ def register():
         elif not email:
             error = 'Email is required.'
         elif db.execute(
-                'SELECT id FROM user WHERE username = ?', (username,)
+                'SELECT id FROM account WHERE username = ?', (username,)
         ).fetchone() is not None:
             error = 'User {} is already registered.'.format(username)
 
         if error is None:
             db.execute(
-                'INSERT INTO user (username, password) VALUES (?, ?)',
-                (username, generate_password_hash(password))
+                'INSERT INTO account (username, password, email) VALUES (?, ?, ?)',
+                (username, generate_password_hash(password), email)
             )
             db.commit()
             return redirect(url_for('login'))
 
         flash(error)
-
         # 提交信息到数据库中
         # form=RegisterForm()
-        account=Account(email,username,password)
-        db.session.add(account)
-        db.session.commit()
-        flash('Your information is saved.')
-
-    # if username == 'admin' and password == '123':
-    #     session['name'] = username
-    #     return redirect(url_for('register'))
-    # if username != 'admin':
-    #     flash('no this name')
+        # account=Account(email,username,password)
+        # db.session.add(account)
+        # db.session.commit()
+        # flash('Your information is saved.')
     return render_template('register.html')
 
 
@@ -122,5 +129,5 @@ def show_house():
 if __name__ == '__main__':
 
     app.run()
-    db.create_all()
+    # db.create_all()
 
